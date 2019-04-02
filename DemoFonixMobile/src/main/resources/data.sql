31cd53de2c8ebfcf84d8a91affce1fe595b91f6a
scugnizzo <massimo_sposato@hotmail.com>
2019-04-02 18:41:11 +0100

    commit

--- DemoFonixMobile/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
- INSERT INTO ITEMS(id, description) VALUES (1, 'Maximum 1 offer daily');
- INSERT INTO ITEMS(id, description) VALUES (2, 'Maximum 2 offers daily');
- INSERT INTO ITEMS(id, description) VALUES (3, 'Maximum 1 offer weekly');
- INSERT INTO ITEMS(id, description) VALUES (4, 'Maximum 1 offer monthly'); 
- INSERT INTO ITEMS(id, description) VALUES (5, 'Uncapped (Always send the best available deal when available)');
